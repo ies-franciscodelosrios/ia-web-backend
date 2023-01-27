132661c8cd8d7ba292e74cc1aab9301bfa38c180
Juan Montañez Palomino <juanmonpa88@gmail.com>
2023-01-27 14:30:55 +0100

    Eventos-Crud

--- src/main/java/apirestful/iawebbackend/controller/EventController.java
+++ src/main/java/apirestful/iawebbackend/controller/EventController.java
@@ -1,13 +1,39 @@
 package apirestful.iawebbackend.controller;
 
+import apirestful.iawebbackend.model.Event;
 import apirestful.iawebbackend.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/event")
 public class EventController {
     @Autowired
     private EventService eventService;
+
+    @GetMapping()
+    public List<Event> obtenerEventos(){
+        return eventService.obtenerEventos();
+    }
+
+
+    //Sirve para guardar y actualizar eventos
+    @PostMapping()
+    public Event guardarEvento(@RequestBody Event evento){
+        return this.eventService.guardarEvento(evento);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Event> obtenerEventoPorId(@PathVariable("id") Long id){
+        return this.eventService.obtenerEventoPorId(id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public boolean eliminarEvento(@PathVariable("id") Long id){
+        return this.eventService.eliminarEvento(id);
+    }
+
 }
--- src/main/java/apirestful/iawebbackend/services/EventService.java
+++ src/main/java/apirestful/iawebbackend/services/EventService.java
@@ -1,12 +1,40 @@
 package apirestful.iawebbackend.services;
 
+import apirestful.iawebbackend.model.Event;
 import apirestful.iawebbackend.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EventService {
     @Autowired
     private EventRepository eventRepository;
+
+    public List<Event> obtenerEventos(){
+        return (List<Event>) eventRepository.findAll();
+    }
+
+    public Event guardarEvento(Event evento){
+        return eventRepository.save(evento);
+    }
+
+    //Para que el método no falle por si no se encuentra el id, le indicamos que es de tipo Optional
+    public Optional<Event> obtenerEventoPorId(Long id){
+        return eventRepository.findById(id);
+    }
+
+    public boolean eliminarEvento(Long id){
+        try{
+            eventRepository.deleteById(id);
+            return true;
+        }catch (Exception err){
+            return false;
+        }
+    }
+
+
 
 }
